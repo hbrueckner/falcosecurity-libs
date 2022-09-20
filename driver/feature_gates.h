a032261f025780bed7b3fa262f5440573cb15663
Hendrik Brueckner <brueckner@de.ibm.com>
2022-09-20 18:30:20 +0200

    new(feature-gates): introduce `socketcall` multiplexer
    Add a feature gate for the `socketcall` multiplexer.
    
    For more details, see `driver/feature_gates.h`.
    
    Signed-off-by: Hendrik Brueckner <brueckner@de.ibm.com>

--- driver/feature_gates.h
+++ driver/feature_gates.h
@@ -65,6 +65,21 @@ or GPL2.txt for full copies of the license.
 #endif
 
 ///////////////////////////////
+// CAPTURE_SOCKETCALL
+///////////////////////////////
+
+/* There are architectures that used history socketcall to multiplex
+ * the network system calls.  Even if architectures, like s390x, has
+ * direct support for those network system calls, kernel version header
+ * dependencies in libc prevent using them.
+ *
+ * For details, see also https://sourceware.org/pipermail/libc-alpha/2022-September/142108.html
+ */
+#if defined(CONFIG_S390)
+	#define CAPTURE_SOCKETCALL
+#endif
+
+///////////////////////////////
 // CAPTURE_SCHED_PROC_EXEC 
 ///////////////////////////////
 
@@ -150,6 +165,14 @@ or GPL2.txt for full copies of the license.
 	#define CAPTURE_SCHED_PROC_FORK 
 #endif
 
+///////////////////////////////
+// CAPTURE_SOCKETCALL
+///////////////////////////////
+
+#if defined(__TARGET_ARCH_s390)
+	#define CAPTURE_SOCKETCALL
+#endif
+
 #else /* Userspace */
 
 /* Please note: the userspace loads the filler table for the bpf probe
@@ -199,6 +222,14 @@ or GPL2.txt for full copies of the license.
 
 #if defined(__aarch64__)
 	#define CAPTURE_SCHED_PROC_EXEC 
+#endif
+
+///////////////////////////////
+// CAPTURE_SOCKETCALL
+///////////////////////////////
+
+#if defined(__s390x__)
+	#define CAPTURE_SOCKETCALL
 #endif
 
 #endif /* UDIG */
